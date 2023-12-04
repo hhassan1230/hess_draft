4317f1af49fcb9edbc0609573866d9beef45e2b6
Hessvacio Hassan <hhassan@oswego.edu>
2023-12-04 14:21:39 -0500

    adds howto, 404 and new privacy policy pages

--- js/404.js
+++ js/404.js
@@ -1,46 +1,46 @@
-var images = [
-	"hess1.png",
-	"hess2.jpg",
-	"hess3.png",
-	"hess4.png",
-	"hess5.jpg",
-	"hess6.png",
-	"hess7.jpg",
-	"hess8.jpg",
-	"hess9.png",
-	"hess10.png",
-	"hess11.png",
-	"hess12.jpg",
-	"hess13.jpg",
-	"hess14.png",
-	"hess15.png",
+const images = [
+    "hess1.png",
+    "hess2.jpg",
+    "hess3.png",
+    "hess4.png",
+    "hess5.jpg",
+    "hess6.png",
+    "hess7.jpg",
+    "hess8.jpg",
+    "hess9.png",
+    "hess10.png",
+    "hess11.png",
+    "hess12.jpg",
+    "hess13.jpg",
+    "hess14.png",
+    "hess15.png",
 ];
 
-var colorComplement = {
-	0 "#62391d",
+const colorComplement = {
+	0: "#62391d",
 	5:"#60ACDD",
 	6:"#F6931E",
 	14:"#6EA9D6",
 	13: "#C5E9F5",
-
 };
 
-var getImgUrl = function(imageName)
-{
-	return "//hessvacio.com/img/hess/" + imageName;
+function getImgUrl(imageName) {
+    return "//hessvacio.com/img/hess/" + imageName;
 }
 
-var getRandomImg = function () {
-	return images[Math.floor(Math.random() * images.length)]
+function getRandomImg () {
+    return images[Math.floor(Math.random() * images.length)];
 }
 
-var getFullPath = function () {
-	return getImgUrl(getRandomImg());
+function getFullPath () {
+    return getImgUrl(this.getRandomImg());
 }
 
-var ChangeUrl = function () {
-	var imgElement = document.getElementById("random-h");
-	imgElement.src = getFullPath();
+function ChangeUrl () {
+    console.log("change url");
+    const photo = getFullPath();
+    console.log(photo);
+    document.getElementById("random-h").src = photo;
 }
 
 ChangeUrl();
