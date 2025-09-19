bdaecfd945bf97a2233e97daf14efa77cd18aedf
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2025-09-19 12:16:13 -0400

    adds leo's code

---
+++ pages/webdevprojects/leosite/javascript/main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+	// Register GSAP Plugins
+  gsap.registerPlugin(ScrollTrigger);
+  // Parallax Layers
+  document.querySelectorAll('[data-parallax-layers]').forEach((triggerElement) => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
+        start: "0% 0%",
+        end: "100% 0%",
+        scrub: 0
+      }
+    });
+    const layers = [
+      { layer: "1", yPercent: 70 },
+      { layer: "2", yPercent: 55 },
+      { layer: "3", yPercent: 40 },
+      { layer: "4", yPercent: 10 }
+    ];
+    layers.forEach((layerObj, idx) => {
+      tl.to(
+        triggerElement.querySelectorAll(`[data-parallax-layer="${layerObj.layer}"]`),
+        {
+          yPercent: layerObj.yPercent,
+          ease: "none"
+        },
+        idx === 0 ? undefined : "<"
+      );
+    });
+  });
+});
+/* Lenis */
+const lenis = new Lenis();
+lenis.on('scroll', ScrollTrigger.update);
+gsap.ticker.add((time) => {lenis.raf(time * 1000);});
+gsap.ticker.lagSmoothing(0);
+
+
+// Exp Year Logic current-year
+
+document.getElementById("year").innerHTML = new Date().getFullYear() - new Date('2018').getFullYear();
+
+document.getElementById("current-year").innerHTML = new Date().getFullYear();
